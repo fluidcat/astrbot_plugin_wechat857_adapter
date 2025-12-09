5b942ab962cf56b482f48e1b1b8238274d1a0201
fluidcat <Belin0@163.com>
2025-12-09 21:22:19 +0800

    更新配置，适配新版本

--- wechat857_adapter.py
+++ wechat857_adapter.py
@@ -106,7 +106,7 @@ class WeChat857Adapter(Platform):
     def __init__(
             self, platform_config: dict, platform_settings: dict, event_queue: asyncio.Queue
     ) -> None:
-        super().__init__(event_queue)
+        super().__init__(platform_config, event_queue)
         self._shutdown_event = None
         self.config = platform_config
         self.settings = platform_settings
